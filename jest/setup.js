05f3d194075d8519c123088da50da9a825bca524
Pawndev <coquelet.c@gmail.com>
2020-08-16 19:23:42 +0200

    test: Mock the google signin library

--- jest/setup.js
+++ jest/setup.js
@@ -12,3 +12,4 @@ jest.mock('react-native-reanimated', () => {
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('@react-native-community/google-signin', () => {});
