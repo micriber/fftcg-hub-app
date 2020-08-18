7d003f05195bda4fa8c3d8748d1678d64445f985
Pawndev <coquelet.c@gmail.com>
2020-08-18 21:18:04 +0200

    test: Add describe

--- __tests__/screens/Home.test.tsx
+++ __tests__/screens/Home.test.tsx
@@ -12,32 +12,33 @@ import {User} from '@react-native-community/google-signin';
 
 // More about testing in this repository:
 // https://github.com/bamlab/react-native-testing/blob/master/src/pages/Home/__tests__/Home.test.tsx
+describe('[Page] Home', () => {
+  it('renders correctly', async () => {
+    const userInfo = {
+      user: {
+        id: '',
+        name: 'Bobby',
+        email: 'bobby.billy@gmail.com',
+        photo: '',
+        familyName: 'Billy',
+        givenName: null,
+      },
+      idToken: null,
+      serverAuthCode: null,
+    };
+    const root = render(
+      <AuthContext.Provider
+        value={{
+          getCurrentUser: (): Promise<User | null> => Promise.resolve(userInfo),
+          signIn: () => Promise.resolve(),
+          signOut: () => Promise.resolve(),
+        }}>
+        <Home />
+      </AuthContext.Provider>,
+    );
 
-it('renders correctly', async () => {
-  const userInfo = {
-    user: {
-      id: '',
-      name: 'Bobby',
-      email: 'bobby.billy@gmail.com',
-      photo: '',
-      familyName: 'Billy',
-      givenName: null,
-    },
-    idToken: null,
-    serverAuthCode: null,
-  };
-  const root = render(
-    <AuthContext.Provider
-      value={{
-        getCurrentUser: (): Promise<User | null> => Promise.resolve(userInfo),
-        signIn: () => Promise.resolve(),
-        signOut: () => Promise.resolve(),
-      }}>
-      <Home />
-    </AuthContext.Provider>,
-  );
-
-  expect(root.toJSON()).toMatchSnapshot(); // Loading screen
-  await waitFor(() => root.getByText('Welcome Bobby'));
-  expect(root.toJSON()).toMatchSnapshot(); // Home page loaded with user
+    expect(root.toJSON()).toMatchSnapshot(); // Loading screen
+    await waitFor(() => root.getByText('Welcome Bobby'));
+    expect(root.toJSON()).toMatchSnapshot(); // Home page loaded with user
+  });
 });
