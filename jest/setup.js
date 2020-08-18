7e2dd9fe0cdc9c089b8e406f37bf4a6438afe1b7
Pawndev <coquelet.c@gmail.com>
2020-08-18 20:56:46 +0200

    test: add test for home page screen

--- jest/setup.js
+++ jest/setup.js
@@ -13,13 +13,18 @@ jest.mock('react-native-reanimated', () => {
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
-jest.mock('@react-native-community/google-signin', () => ({
-  GoogleSignin: {
-    configure: jest.fn(),
-    hasPlayServices: jest.fn(() => Promise.resolve(true)),
-    signIn: jest.fn(() => Promise.resolve(mockUserInfo)),
-    signInSilently: jest.fn(() => Promise.resolve(mockUserInfo)),
-    revokeAccess: jest.fn(() => Promise.resolve(true)),
-    signOut: jest.fn(() => Promise.resolve(true)),
+jest.mock('react-native-appearance', () => {
+  return require('react-native-appearance/src/mock');
+});
+jest.mock('@react-native-community/google-signin', () => {
+  return {
+    GoogleSignin: {
+      configure: jest.fn(),
+      hasPlayServices: jest.fn(() => Promise.resolve(true)),
+      signIn: jest.fn(() => Promise.resolve(mockUserInfo)),
+      signInSilently: jest.fn(() => Promise.resolve(mockUserInfo)),
+      revokeAccess: jest.fn(() => Promise.resolve(true)),
+      signOut: jest.fn(() => Promise.resolve(true)),
+    }
   }
-}));
+});
