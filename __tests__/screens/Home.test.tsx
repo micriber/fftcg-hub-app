c63d795b5cdaf6fe561ce402fa7f172e2900a8da
Pawndev <coquelet.c@gmail.com>
2020-08-22 18:35:18 +0200

    test: Update test and snapshot for home screen

--- __tests__/screens/Home.test.tsx
+++ __tests__/screens/Home.test.tsx
@@ -6,39 +6,14 @@ import 'react-native';
 import React from 'react';
 import Home from '../../src/screens/Home';
 // import renderer from 'react-test-renderer';
-import {render, waitFor} from '@testing-library/react-native';
-import {AuthContext} from '../../src/AuthContext';
-import {User} from '@react-native-community/google-signin';
+import {render} from '@testing-library/react-native';
 
 // More about testing in this repository:
 // https://github.com/bamlab/react-native-testing/blob/master/src/pages/Home/__tests__/Home.test.tsx
 describe('[Page] Home', () => {
   it('renders correctly', async () => {
-    const userInfo = {
-      user: {
-        id: '',
-        name: 'Bobby',
-        email: 'bobby.billy@gmail.com',
-        photo: '',
-        familyName: 'Billy',
-        givenName: null,
-      },
-      idToken: null,
-      serverAuthCode: null,
-    };
-    const root = render(
-      <AuthContext.Provider
-        value={{
-          getCurrentUser: (): Promise<User | null> => Promise.resolve(userInfo),
-          signIn: () => Promise.resolve(),
-          signOut: () => Promise.resolve(),
-        }}>
-        <Home />
-      </AuthContext.Provider>,
-    );
+    const root = render(<Home />);
 
-    expect(root.toJSON()).toMatchSnapshot(); // Loading screen
-    await waitFor(() => root.getByText('Welcome Bobby'));
-    expect(root.toJSON()).toMatchSnapshot(); // Home page loaded with user
+    expect(root.toJSON()).toMatchSnapshot(); // Home page loaded with cards
   });
 });
