0600599ea1beb436397b339ea2ea8a6e8857e5ab
bersiroth <caronbernard1@gmail.com>
2021-05-01 11:02:06 +0200

    feat: search form integration

--- src/components/common/form-fields/SearchInput.tsx
+++ src/components/common/form-fields/SearchInput.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import {TextInput} from 'react-native-paper';
+import {TextInput, withTheme} from 'react-native-paper';
 import {TextInputProps} from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
 import {StyleSheet} from 'react-native';
+import {ColorTranslator} from 'colortranslator';
 
-// Here, we exclude theme
-// The TextInputProps required it, but it come with a `withTheme`
-// inside de `TextInput` component, we don't have access to it from here
-// We could, but not interesting atm.
-type Props = {} & Omit<TextInputProps, 'theme'>;
+type Props = {} & TextInputProps;
 
-const SearchInput = (props: Props) => (
-  <TextInput
-    blurOnSubmit
-    autoCapitalize="none"
-    placeholder={props.placeholder}
-    autoCorrect={props.autoCorrect || false}
-    style={[styles.searchInput, props.style]}
-    value={props.value}
-    onChangeText={props.onChangeText}
-    mode={props.mode || 'outlined'}
-    label={props.label}
-  />
-);
+const SearchInput = (props: Props) => {
+  const styles = StyleSheet.create({
+    searchInput: {
+      marginVertical: 10,
+      backgroundColor: new ColorTranslator(props.theme.colors.active).setA(0.2)
+        .RGBA,
+      fontSize: 17,
+    },
+  });
+  return (
+    <TextInput
+      blurOnSubmit
+      autoCapitalize="none"
+      placeholder={props.placeholder}
+      autoCorrect={props.autoCorrect || false}
+      style={[styles.searchInput, props.style]}
+      value={props.value}
+      onChangeText={props.onChangeText}
+      mode={props.mode || 'flat'}
+      label={props.label}
+      theme={props.theme}
+      dense={true}
+    />
+  );
+};
 
-const styles = StyleSheet.create({
-  searchInput: {
-    margin: 10,
-  },
-});
-
-export default SearchInput;
+export default withTheme(SearchInput);
--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchInput from '../common/form-fields/SearchInput';
-import {Button} from 'react-native-paper';
+import {Button, useTheme} from 'react-native-paper';
 import {StyleSheet, View, ViewStyle} from 'react-native';
 
 export type SubmitParams = {
@@ -14,27 +14,35 @@ type Props = {
 
 const SearchForm = (props: Props) => {
   const [search, setSearch] = React.useState('');
+  const {colors} = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      marginHorizontal: 10,
+    },
+    buttonLabel: {
+      color: colors.lightGrey,
+      fontWeight: 'bold',
+    },
+    button: {
+      elevation: 2,
+    },
+  });
   return (
     <View style={[styles.container, props.style]}>
       <SearchInput
-        placeholder={'Rechercher "1-001R", "Séphiroth"'}
         value={search}
         onChangeText={setSearch}
         label={'Nom de carte ou code'}
       />
-      <Button mode="contained" onPress={() => props.onSubmit({search})}>
-        Submit
+      <Button
+        mode="contained"
+        onPress={() => props.onSubmit({search})}
+        style={styles.button}
+        labelStyle={styles.buttonLabel}>
+        Rechercher
       </Button>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    // marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-});
 
 export default SearchForm;
