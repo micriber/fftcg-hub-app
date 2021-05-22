52bfbf250f55188eae905a323556b009c06104b7
bersiroth <caronbernard1@gmail.com>
2021-05-22 18:07:37 +0200

    add eslint to CI

--- src/utils/hooks/useAsync.ts
+++ src/utils/hooks/useAsync.ts
@@ -4,9 +4,7 @@ import {FnReturningPromise} from './util';
 
 export type {AsyncState, AsyncFnReturn} from './useAsyncFn';
 
-export default function useAsync<T extends FnReturningPromise>(
-  fn: T
-) {
+export default function useAsync<T extends FnReturningPromise>(fn: T) {
   const [state, callback] = useAsyncFn(fn, {
     loading: true,
   });
