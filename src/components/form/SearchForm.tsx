59572cba225463fbd6875cf7c59ef04f83893625
bersiroth <caronbernard1@gmail.com>
2021-05-29 15:44:25 +0200

    feat: add filters

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SearchInput from '../common/form-fields/SearchInput';
-import {Button, useTheme} from 'react-native-paper';
-import {StyleSheet, View, ViewStyle} from 'react-native';
+import {Button, useTheme, Text, Switch} from 'react-native-paper';
+import {Keyboard, StyleSheet, View, ViewStyle} from 'react-native';
 
 export type SubmitParams = {
   search: string;
+  owned: boolean;
 };
 
 type Props = {
@@ -14,6 +15,7 @@ type Props = {
 
 const SearchForm = (props: Props) => {
   const [search, setSearch] = React.useState('');
+  const [owned, setOwned] = React.useState(false);
   const {colors} = useTheme();
   const styles = StyleSheet.create({
     container: {
@@ -27,6 +29,20 @@ const SearchForm = (props: Props) => {
     button: {
       elevation: 2,
     },
+    filterRowContainer: {
+      flexDirection: 'row',
+      height: 24,
+      marginBottom: 10,
+    },
+    collectionContainer: {
+      flexDirection: 'row',
+      width: '60%',
+      alignItems: 'flex-end',
+    },
+    filterButtonContainer: {
+      width: '40%',
+      alignItems: 'flex-end',
+    },
   });
   return (
     <View style={[styles.container, props.style]}>
@@ -35,9 +51,25 @@ const SearchForm = (props: Props) => {
         onChangeText={setSearch}
         label={'Nom de carte ou code'}
       />
+      <View style={styles.filterRowContainer}>
+        <View style={styles.collectionContainer}>
+          <Text style={{marginBottom: 3, color: colors.accent, fontSize: 15, marginRight: 5}}>Collection uniquement</Text>
+          <Switch
+            value={owned}
+            onValueChange={(withValue: boolean) => {
+              Keyboard.dismiss();
+              setOwned(withValue);
+            }}
+            color={colors.active}
+          />
+        </View>
+        <View style={styles.filterButtonContainer}>
+          <Button style={{marginTop: -7}} labelStyle={{fontSize: 15, fontWeight: 'bold'}} mode="text">FILTRES V</Button>
+        </View>
+      </View>
       <Button
         mode="contained"
-        onPress={() => props.onSubmit({search})}
+        onPress={() => props.onSubmit({search, owned})}
         style={styles.button}
         labelStyle={styles.buttonLabel}>
         Rechercher
