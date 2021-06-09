dad02044baa76e1326d7a4abaa82eacf7e228813
bersiroth <caronbernard1@gmail.com>
2021-06-09 09:47:36 +0200

    feat: add placeholder card image

--- src/screens/Cards/CardDetail.tsx
+++ src/screens/Cards/CardDetail.tsx
@@ -18,6 +18,7 @@ import {makeID} from '../../utils/string';
 import GameIcon from '../../components/icons/GameIcon';
 import {getElementIconFileByElement} from '../../enums/element';
 import {categoriesData, raritiesData} from '../../components/form/SearchForm';
+import FastImage from 'react-native-fast-image';
 
 type Props = {
   route: {params: {card: Card}};
