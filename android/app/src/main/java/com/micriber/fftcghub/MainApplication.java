c26d6fe4c560dce84dd703600a2aa18ec8b1aa67
bersiroth <caronbernard1@gmail.com>
2021-07-06 17:26:53 +0200

    chore: rename app (#129)
    * chore: rename app

--- android/app/src/main/java/com/micriber/fftcghub/MainApplication.java
+++ android/app/src/main/java/com/micriber/fftcghub/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fftcgcollection;
+package com.micriber.fftcghub;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fftcgcollection.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.micriber.fftcghub.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
