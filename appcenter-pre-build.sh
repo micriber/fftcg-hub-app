b94e5457c8b698ef2d4b982a3a091a226e8a169d
bersiroth <caronbernard1@gmail.com>
2021-07-23 21:15:52 +0200

    tech: add sentry config to appcenter

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,18 +6,12 @@ echo $APPCENTER_CONFIG_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/and
 echo "Updating Google JSON"
 echo $GOOGLE_SERVICES_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json"
 
-envsubst < "$APPCENTER_SOURCE_DIRECTORY/.env.dist" > "$APPCENTER_SOURCE_DIRECTORY/.env"
+echo "Updating sentry"
+echo $SENTRY_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/sentry.properties"
 
 echo "Updating firebase"
 echo $FIREBASE_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/firebase.json"
 
-#echo "Updating Google plist"
-#echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/GoogleService-Info.plist"
-
-#echo "Updating android secret"
-#echo $ANDROID_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/assets/appcenter-config.json"
-
-#echo "Updating iOS secret"
-#echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/Glitz/AppCenter-Config.plist"
+envsubst < "$APPCENTER_SOURCE_DIRECTORY/.env.dist" > "$APPCENTER_SOURCE_DIRECTORY/.env"
 
 echo "Finished injecting secrets."
